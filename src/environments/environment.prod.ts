bb31aa366db844a746e5bfe228a723b397d86ad7
bozzaccio <74464364+bozzaccio@users.noreply.github.com>
2021-01-18 10:57:01 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   api: {
     protocol: 'http',
-    hostname: 'localhost',
-    port: '8080',
+    hostname: 'stats-project-backend.herokuapp.com',
+    port: '',
     context: 'api'
   }
 };
